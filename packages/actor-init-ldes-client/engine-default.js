a791c558520c1c0aa56595c599f4c8d0d75d8bf1
brechtvdv <br5cht@hotmail.com>
2021-01-12 13:13:58 +0100

    First version that builds with Comunica

---
+++ packages/actor-init-ldes-client/engine-default.js
@@ -0,0 +1,3 @@
+const urn_comunica_my = new (require('./lib/LDESClient').LDESClient)();
+module.exports = urn_comunica_my;
+
--- packages/actor-init-ldes/index.ts
+++
@@ -1,4 +0,0 @@
-import { LDESClient } from './lib/LDESClient';
-export * from "./lib/LDESClient";
-// tslint:disable:no-var-requires
-export default <LDESClient> require('./engine-default');
