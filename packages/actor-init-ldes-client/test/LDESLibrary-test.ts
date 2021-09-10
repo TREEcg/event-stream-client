462d8e5b8d30162c74435a2d739cf0687507e83a
Pieter Colpaert <pieter.colpaert@ugent.be>
2021-09-10 12:44:08 +0200

    Added test-case to check whether stream ends

--- packages/actor-init-ldes-client/test/LDESLibrary-test.ts
+++ packages/actor-init-ldes-client/test/LDESLibrary-test.ts
@@ -1,15 +1,14 @@
 import { newEngine } from '@treecg/actor-init-ldes-client';
 import { Quad } from "@rdfjs/types";
 
-
-
 describe('LDESClient as a lib', () => {
     var LDESClient = newEngine();
     test('Stream should emit strings when configured that way', (done) => {
         try {
             let url = "https://semiceu.github.io/LinkedDataEventStreams/example.ttl";
             let options = {
-                "mimeType": "text/turtle"
+                "mimeType": "text/turtle",
+                "disablePolling" : true
             };
             let eventstreamSync = LDESClient.createReadStream(url, options);
             eventstreamSync.once('data', (data) => {
@@ -25,9 +24,9 @@ describe('LDESClient as a lib', () => {
         try {
             let url = "https://semiceu.github.io/LinkedDataEventStreams/example.ttl";
             let options = {
-                "representation": "Quads"
+                "representation": "Quads",
+                "disablePolling" : true
             };
-            let LDESClient = newEngine();
             let eventstreamSync = LDESClient.createReadStream(url, options);
             eventstreamSync.once('data', (member) => {
                 expect(member.quads).toBeInstanceOf(Array);
@@ -42,9 +41,9 @@ describe('LDESClient as a lib', () => {
         try {
             let url = "https://semiceu.github.io/LinkedDataEventStreams/example.ttl";
             let options = {
-                "representation": "Object"
+                "representation": "Object",
+                "disablePolling" : true
             };
-            let LDESClient = newEngine();
             let eventstreamSync = LDESClient.createReadStream(url, options);
             eventstreamSync.once('data', (member) => {
                 expect(member.object).toBeInstanceOf(Object);
@@ -52,6 +51,28 @@ describe('LDESClient as a lib', () => {
             });
         } catch (e) {
             done(e);
+        }
+    });
+
+    test('The stream should end when done', (done) => {
+        try {
+            let url = "https://semiceu.github.io/LinkedDataEventStreams/example.ttl";
+            let options = {
+                "representation": "Object",
+                "disablePolling" : true
+            };
+            let eventstreamSync = LDESClient.createReadStream(url, options);
+            const mock = jest.fn();
+            eventstreamSync.on("data", (member) => {
+                //do nothing
+            })
+            eventstreamSync.on('end', () =>  {
+                mock();
+                done();
+            });
+            expect(mock).toHaveBeenCalled;
+        } catch (e) {
+            //done(e);
         }
     });
 });
