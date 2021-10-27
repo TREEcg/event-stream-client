88106565ee1f68baf24e1a3d537c578dd9e36a84
Kasper Zutterman <kasperzutterman1999@gmail.com>
2021-10-27 10:59:14 +0200

    feat: export interface State from EventStream

--- packages/actor-init-ldes-client/index.ts
+++ packages/actor-init-ldes-client/index.ts
@@ -1,7 +1,7 @@
 import { LDESClient } from './lib/LDESClient';
 
 export * from "./lib/LDESClient";
-export { EventStream } from "./lib/EventStream";
+export { EventStream, State } from "./lib/EventStream";
 // tslint:disable:no-var-requires
 //export default <LDESClient> require('./engine-default');
 
